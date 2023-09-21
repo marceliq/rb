5e3a971829d22ed63bd677be5a4e8b757c53a8f1
marceliq <mhnilka@gmail.com>
2023-09-21 07:02:35 +0000

    rename

---
+++ apache24-2.4.55/buildlink3.mk
@@ -0,0 +1,34 @@
+# $NetBSD: buildlink3.mk,v 1.16 2022/06/28 11:37:02 wiz Exp $
+
+BUILDLINK_TREE+=	apache
+
+.if !defined(APACHE_BUILDLINK3_MK)
+APACHE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.apache+=	apache>=2.4.7<2.5
+BUILDLINK_ABI_DEPENDS.apache+=	apache>=2.4.54nb1
+BUILDLINK_PKGSRCDIR.apache?=	../../rb/apache24
+BUILDLINK_DEPMETHOD.apache?=	build
+.if defined(APACHE_MODULE)
+BUILDLINK_DEPMETHOD.apache+=	full
+.endif
+
+BUILDLINK_FILES.apache+=	bin/apxs
+${BUILDLINK_DIR}/bin/apxs: buildlink-directories
+	${MKDIR} ${BUILDLINK_DIR}/bin && ${LN} -fs ../bin/apxs ${BUILDLINK_DIR}/bin/apxs
+
+buildlink-apache-cookie: ${BUILDLINK_DIR}/bin/apxs
+
+USE_TOOLS+=		perl			# for "apxs"
+CONFIGURE_ENV+=		APR_LIBTOOL=${LIBTOOL:Q}	# make apxs use the libtool we specify
+MAKE_ENV+=		APR_LIBTOOL=${LIBTOOL:Q}
+APXS?=			${BUILDLINK_PREFIX.apache}/bin/apxs
+.if defined(GNU_CONFIGURE) && ${GNU_CONFIGURE_APXS2:Uyes} == yes
+CONFIGURE_ARGS+=	${CONFIGURE_ARGS.apache:U--with-apxs2=${APXS:Q}}
+.endif
+
+.include "../../devel/apr/buildlink3.mk"
+.include "../../devel/apr-util/buildlink3.mk"
+.endif # APACHE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-apache
